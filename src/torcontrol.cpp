94827a14b2ca3001a71837d302ff56c9d04b32d5
Roger Taylor <roger.taylor.email@gmail.com>
2020-08-10 14:03:13 +1200

    Update server.cpp and torcontrol.cpp for boost bind placeholders change, to remove the warnings. These aren't as critical as validation.cpp or calidationinterface.cpp, which caused build errors on Windows and MacOS.

--- src/torcontrol.cpp
+++ src/torcontrol.cpp
@@ -18,7 +18,7 @@
 #include <boost/algorithm/string/predicate.hpp>
 #include <boost/algorithm/string/replace.hpp>
 #include <boost/algorithm/string/split.hpp>
-#include <boost/bind.hpp>
+#include <boost/bind/bind.hpp>
 #include <boost/signals2/signal.hpp>
 
 #include <event2/buffer.h>
@@ -26,6 +26,8 @@
 #include <event2/event.h>
 #include <event2/thread.h>
 #include <event2/util.h>
+
+using namespace boost::placeholders;
 
 /** Default control port */
 const std::string DEFAULT_TOR_CONTROL = "127.0.0.1:9051";
